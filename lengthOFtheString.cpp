9329c65d7b51f1e96f2376d7d0e21c574ff9648b
nitin-jaswal24 <94535054+nitin-jaswal24@users.noreply.github.com>
2022-08-05 18:03:21 +0530

    Add files via upload

---
+++ lengthOFtheString.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string s="Nitin Jaswal";
+    int i=0;
+    while(s[i]!='\0')
+    {
+
+        cout<<s[i]<<" ";
+        i++;
+    }
+}
