39c36f10028e2fe6949dc476e94845df9ca1d785
nitin-jaswal24 <94535054+nitin-jaswal24@users.noreply.github.com>
2022-08-07 21:31:57 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,68 +1,73 @@
-
-
-
-
-
-
-
-
-
-
-
 #include <iostream>
+
 using namespace std;
 
-class Array{
+struct node
+{
+    int data;
+    node *next;
 
-private:
-    int* A;
-    int size;
-    int length;
-
-public:
-    Array(int size){
-        this->size = size;
-        A = new int [size];
-    }
-
-    void create(){
-        cout << "Enter number of elements: " << flush;
-        cin >> length;
-        cout << "Enter the array elements: " << endl;
-        for (int i = 0; i < length; i++){
-            cout << "Array element: " << i << " = " << flush;
-            cin >> A[i];
-        }
-    }
-
-    void display(){
-        for (int i = 0; i < length; i++){
-            cout << A[i] << " ";
-        }
-    }
-    void Insert(Array &arr,int index)
+}*first=NULL;
+void create(int arr[],int n)
+{
+    int i;
+    node *t,*last;
+    first=new node;
+    first->data=arr[0];
+    first->next=NULL;
+    last=first;
+    for(i=1;i<n;i++)
     {
-        for(int i=length;i>index;i--)
-        {
-            A[i]=A[i-1];
-        }
-        A[index]=100;
+        t=new node;
+        t->data=arr[i];
+        t->next=NULL;
+        last->next=t;
+        last=t;
+
     }
 
-//    ~Array(){
-//        delete[] A;
-//        cout << "Array destroyed" << endl;
-//    }
-};
-
-int main() {
+}
 
 
-    Array arr(10);
-    arr.create();
-    arr.Insert( arr,5);
-    arr.display();
+int count(node *p)
+{
 
+    if(p==0)
+    {
+        return 0;
 
+    }
+    return count(p->next)+1;
+}
+
+int add(node *p)
+{
+    int sum=0;
+    if(p==0)
+    {
+        return 0;
+    }
+    else{
+        return p->data+add(p->next);
+    }
+}
+
+void display(node *p)
+{
+    while(p!=NULL)
+    {
+        cout<<p->data<<endl;
+        p=p->next;
+    }
+}
+
+int main()
+{
+    int A[]={3,4,5,6,7,8,9};
+    create(A,7);
+    cout<<count(first)<<endl;
+    cout<<add(first)<<endl;
+
+    display(first);
     return 0;
 }
