e9d03f4f3e65e596d944a9794ec9ad38b12b83a6
nitin-jaswal24 <94535054+nitin-jaswal24@users.noreply.github.com>
2022-08-13 12:41:57 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -18,15 +18,7 @@ struct node
     int data;
 
     node *next;
-
-
-
-
-
 }*first=NULL,*last=NULL; //changes
-
-
-
 
 
 // code for insertion in the linked list
@@ -49,15 +41,9 @@ void Insert(node *n,int index,int val)
 
          if(first==NULL){
              last=p;
-         }
-
-
-
-     }
-
-
-
-        node *p=new node;
+         }}
+     else{
+         node *p=new node;
 
      p->data=val;
 
@@ -67,24 +53,13 @@ void Insert(node *n,int index,int val)
 
      {
 
-         n=n->next;
-
-     }
+         n=n->next;}
      if(!n->next){
          last=p;
      }
      p->next=n->next;
 
-     n->next=p;
-
-
-
-
-
-
-
-
-
+     n->next=p;}
 }
 
 
@@ -125,10 +100,104 @@ void insertLast(int val)
 
 }
 
+
+
+//now delting the node
+
+//first deleete the firstNode
+//
+//void deleteFirst(node *p)
+//{
+//    p=first;
+//    first=first->next;
+//    delete p;
+//}
+
+// lets delte the specified node
+
+void deleteNode(node *p,int index)
+{
+    if(index==0)
+    {
+        p=first;
+        first=first->next;
+        delete p;
+    }
+    else{
+        p=first;
+        node *q=nullptr;
+        for(int i=0;i<index-1&&p;i++)
+        {   q=p;
+            p=p->next;
+
+
+        }
+        if(p->next==nullptr&&p)
+        {
+            q->next=nullptr;
+            last=q;
+            delete p;
+        }
+        else if(p){
+            q->next=p->next;
+        delete p;
+        }
+
+
+    }
+
+}
+
+
+
+// lets check linked list is sorted or not
+
+bool isSorted(node *p)
+{
+    node *q=nullptr;
+    p=first;
+    while(p)
+    {
+        q=p;
+        p=p->next;
+
+        if(p&&p->data<q->data)
+        {
+            return false;
+        }
+
+
+    }
+    return true;
+
+
+
+}
+
+
+// now lets remove the duplicates from the linked list
+void removeDuplicate(node *p)
+{
+
+    node *q=p->next;
+    while(q!=nullptr)
+    {
+
+
+        if(p->data!=q->data)
+        {
+            p=q;
+            q=q->next;
+        }
+        else{
+            p->next=q->next;
+            delete q;
+            q=p->next;
+        }
+    }
+
+}
 void display(node *p)
-
-
-
 {
 
         while(p!=NULL)
@@ -145,34 +214,6 @@ void display(node *p)
 
 }
 
-//inserting if the linked list is sorted
-
-void insertSorted(node *n,int index,int value)
-{
-    node*p=first;
-    node *q=nullptr;
-    if(p->data<value)
-    {
-        while(p && p->data<value)
-    {
-        q=p;
-        p=p->next;
-    }
-    node *t=new node;
-    t->data=value;
-    t->next=q->next;
-    q->next=t;
-    }
-    else
-    {
-        node *t=new node;
-        t->data=value;
-        t->next=first;
-        first=t;
-
-    }
-
-}
 
 
 
@@ -187,16 +228,20 @@ int main()
 
 Insert(first,0,1);
 
-Insert(first,1,99);
+ Insert(first,1,99);
 
-Insert(first,2,199);
+ Insert(first,2,199);
 
-Insert(first,3,999);
-
-// insertLast(10009);
-insertSorted(first,0,0);
+ Insert(first,3,999);
+ Insert(first,4,1000);
+ Insert(first,5,2000);
+  Insert(first,6,2000);
+  Insert(first,6,2000);
 
 display(first);
+//cout<<isSorted(first)<<endl;
+ removeDuplicate(first);
+ display(first);
 
 
 
