230f3630b7f5862e69785cf57349588ac0787599
nitin-jaswal24 <94535054+nitin-jaswal24@users.noreply.github.com>
2022-08-21 15:03:16 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,144 +1,176 @@
 #include <iostream>
-
 using namespace std;
-
-struct node
+class Node;
+Node *last=nullptr;
+Node *first=nullptr;
+class Node
 {
+public:
+
     int data;
-    node *next;
+    Node *next;
+    // void insertEmpty(Node *last,int value)
 
-}*first=NULL,*second=nullptr,*third=nullptr;
-void create1(int arr[],int n)
-{
-    int i;
-    node *t,*last;
-    first=new node;
-    first->data=arr[0];
-    first->next=NULL;
-    last=first;
-    for(i=1;i<n;i++)
+    // {
+    //     if(last!=nullptr)
+    //     {
+    //         return;
+    //     }
+
+    //     Node *temp=new Node;
+    //     temp->data=value;
+    //     ::last=temp;
+    //     first=temp;
+    //     temp->next=last;
+
+    // }
+
+    //lets insert at the last node and make last node pointing on the first node
+    // void insertLast(Node *last,int value)
+    // {
+    //     if(last==nullptr)
+    //     {
+    //         insertEmpty(last,value);
+    //     }
+
+    //     Node *temp=new Node;
+    //     temp->data=value;
+    //     temp->next=last->next;
+    //     last->next=temp;
+    //     ::last=temp;
+
+    // }
+    // void insertPos(Node *p,int index,int data)
+    // {
+
+    //     for(int i=0;i<index-1;i++)
+    //     {
+    //         p=p->next;
+    //     }
+    //     Node *temp=new Node;
+    //     temp->data=data;
+    //     temp->next=p->next;
+    //     p->next=temp;
+
+
+    // }
+
+    void insertNode(Node *p,int data,int index)
     {
-        t=new node;
-        t->data=arr[i];
-        t->next=NULL;
-        last->next=t;
-        last=t;
-
-    }
-
-}
-
-
-int count(node *p)
-{
-
-    if(p==0)
-    {
-        return 0;
-
-    }
-    return count(p->next)+1;
-}
-void create2(int arr[],int n)
-{
-    int i;
-    node *t,*last;
-    second=new node;
-    second->data=arr[0];
-    second->next=NULL;
-    last=second;
-    for(i=1;i<n;i++)
-    {
-        t=new node;
-        t->data=arr[i];
-        t->next=NULL;
-        last->next=t;
-        last=t;
-
-    }
-
-}
-
-
-
-
-int add(node *p)
-{
-    int sum=0;
-    if(p==0)
-    {
-        return 0;
-    }
-    else{
-        return p->data+add(p->next);
-    }
-}
-
-void display(node *p)
-{
-    while(p!=NULL)
-    {
-        cout<<p->data<<" ";
-        p=p->next;
-    }
-    cout<<endl;
-}
-
-
-//lets merge the 2 linked list
-void mergeList(node *p,node *q)
-{
-    node *last;
-    if(p->data>q->data)
-    {
-        third=last=q;
-        q=q->next;
-        third->next=nullptr;
-    }
-    else{
-        third=last=p;
-        p=p->next;
-        third->next=nullptr;
-    }
-    while(p!=nullptr && q!=nullptr)
-    {
-        if(p->data>q->data)
+        if(index<0)
         {
-            last->next=q;
-            last=q;
-            q=q->next;
-            last->next=nullptr;
+            return;
+        }
+        // Node *temp=new Node;
+        if(index==0)
+        {   Node *q=first;
+
+            Node *temp=new Node;
+            if(first==nullptr)
+            {   temp->data=data;
+
+                first=temp;
+                temp->next=first;
+
+            }
+            else{
+                temp->data=data;
+                temp->next=first;
+                while(q->next!=first)
+                {
+                    q=q->next;
+                }
+                temp->next=first;
+                q->next=temp;
+                first=temp;
+
+            }
         }
         else{
-            last->next=p;
-            last=p;
-            p=p->next;
-            last->next=nullptr;
+            Node *q=first;
+            for(int i=0;i<index-1;i++)
+            {
+                q=q->next;
+            }
+            Node *temp=new Node;
+            temp->data=data;
+            temp->next=q->next;
+            q->next=temp;
         }
     }
-    if(p!=nullptr)
+
+    //delete the node
+
+    void deleteNode(Node *first,int index)
     {
-        last->next=p;
-    }
-    if(q!=nullptr)
+        Node *p;
+        Node *q;
+        if(index==1)
     {
-        last->next=q;
+        p=first;
+        while(p->next!=first)
+        {
+            p=p->next;
+        }
+        if(p==first)
+        {
+            delete first;
+            first=nullptr;
+        }
+        else{
+        p->next=first->next;
+        delete first;
+        first=p->next;
+
+        }
+    }
+
+    else{
+        p=first;
+        for(int i=0;i<index-2;i++)
+        {
+            p=p->next;
+        }
+        q=p->next;
+        p->next=q->next;
+        delete q;
 
     }
 
-}
+    }
 
+    // traverse the whole linked list
+    void display(Node *p)
+    {
+        if(p==nullptr)
+        {
+            return;
+        }
+        do
+        {
+            cout<<p->data<<" ";
+            p=p->next;
+        }while(p!=first);
+    }
+
+};
 int main()
 {
-    int A[]={10,20,30,40,50};
-    int B[]={5,15,25,35,45};
-    create1(A,7);
-    create2(B,7); // 2 linked list has been created
+    Node *r=new Node;
+    // r->insertEmpty(last,12);
+    // r->insertLast(last,10);
+    // r->insertLast(last,100);
+    r->insertNode(first,100,0);
+    r->insertNode(first,100,1);
+    r->insertNode(first,200,0);
+    r->insertNode(first,4,3);
+    r->display(first);
+    cout<<endl;
+    r->deleteNode(first,4);
 
-//    display(first);
-//    display(second);
-    mergeList(first,second);
-    display(third);
-    return 0;
+    r->display(first);
 }
+
+
+
 
