9bac580f876abb54b8ec040561e0bb89ba62089e
nitin-jaswal24 <94535054+nitin-jaswal24@users.noreply.github.com>
2022-10-08 18:14:55 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,55 +1,112 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
 
 #include <iostream>
-#include <queue>
+
 using namespace std;
-class Stack
-{
-    public:
-            queue <int> q;
 
-    void push(int data)
-    {
-        q.push(data);
-        int s=q.size();
-
-        for(int i=0;i<s-1;i++)
-        {
-            q.push(q.front());
-            q.pop();
-        }
-
-    }
-    int pop()
-    {
-        int x=q.front();
-        q.pop();
-        return x;
-    }
-    int top()
-    {
-        return q.front();
-    }
-    int size()
-    {
-        return q.size();
-    }
+class Node{
+public:
+    Node* lchild;
+    int data;
+    Node* rchild;
 };
-int main()
-{
-    Stack s;
-    s.push(23);
-    s.push(24);
-    s.push(25);
-    s.push(26);
 
-    cout<<s.pop()<<endl;
+class BST{
+private:
+    Node* root;
+public:
+    BST(){ root = nullptr; }
+    Node* getRoot(){ return root; }
+    void Insert(int key);
+    void Inorder(Node* p);
+    Node* Search(int key);
+};
+
+void BST::Insert(int key) {
+
+    Node* t = root;
+    Node* p;
+    Node* r;
+
+    // root is empty
+    if (root == nullptr){
+        p = new Node;
+        p->data = key;
+        p->lchild = nullptr;
+        p->rchild = nullptr;
+        root = p;
+        return;
+    }
+
+    while(t != nullptr){
+        r = t;
+        if (key < t->data){
+            t = t->lchild;
+        } else if (key > t->data){
+            t = t->rchild;
+        } else {
+            return;
+        }
+    }
+
+    // Now t points at NULL and r points at insert location
+    p = new Node;
+    p->data = key;
+    p->lchild = nullptr;
+    p->rchild = nullptr;
+
+    if (key < r->data){
+        r->lchild = p;
+    } else {
+        r->rchild = p;
+    }
 
 }
 
+void BST::Inorder(Node* p) {
+    if (p){
+        Inorder(p->lchild);
+        cout << p->data << ", " << flush;
+        Inorder(p->rchild);
+    }
+}
+
+Node* BST::Search(int key) {
+    Node* t = root;
+    while (t != nullptr){
+        if (key == t->data){
+            return t;
+        } else if (key < t->data){
+            t = t->lchild;
+        } else {
+            t = t->rchild;
+        }
+    }
+    return nullptr;
+}
+
+
+int main() {
+
+    BST bst;
+
+    // Insert
+    bst.Insert(10);
+    bst.Insert(5);
+    bst.Insert(20);
+    bst.Insert(8);
+    bst.Insert(30);
+
+    // Inorder traversal
+    bst.Inorder(bst.getRoot());
+    cout << endl;
+
+    // Search
+    Node* temp = bst.Search(2);
+    if (temp != nullptr){
+        cout << temp->data << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+
+    return 0;
+}
